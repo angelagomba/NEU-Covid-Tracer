77380765f723027f61da02a231b683f6b942420e
Amiel Monasterial <amielmon@gmail.com>
2020-12-16 22:45:01 -0500

    self screen

--- components/SelfScreening/index.tsx
+++ components/SelfScreening/index.tsx
@@ -7,15 +7,73 @@ import * as WebBrowser from 'expo-web-browser'
 import { 
   Text, 
   View,
-  ScrollView 
+  ScrollView,
+  CheckBox
 } from 'react-native'
 
 function SelfScreening({navigation, route}) {
+  
+  const question1 = () => {
+    return ( 
+      <View style={SelfScreeningStyles.question}>
+        <Text style={SelfScreeningStyles.questionTitle}>Question 1</Text>
+        <Text style={SelfScreeningStyles.questionCTA}>Are you experiencing any of the following symptoms?</Text>
+        <View style={SelfScreeningStyles.question1questions}>
+          <Text style={{fontSize: 18}}>o New loss of smell or taste</Text>
+          <Text style={{fontSize: 18}}>o New/worsened muscle aches</Text>
+          <Text style={{fontSize: 18}}>o New/worsened cough</Text>
+          <Text style={{fontSize: 18}}>o New/worsened shortness of breath</Text>
+          <Text style={{fontSize: 18}}>o New/worsened sore throat</Text>
+          <Text style={{fontSize: 18}}>o Diarrhea/vomitting</Text>
+          <Text style={{fontSize: 18}}>o Fever, feeling feverish or shaking chills</Text>
+        </View>
+      </View>
+    )
+  }
+
+  const question2 = () => {
+    return (
+      <View style={SelfScreeningStyles.question}>
+        <Text style={SelfScreeningStyles.questionTitle}>Question 2</Text>
+        <Text style={SelfScreeningStyles.questionCTA}>In the past fourteen (14) days, have you had close contact with someone who is confirmed as having COVID-19?</Text>
+        <Text style={SelfScreeningStyles.question2desc}>A close contact is defined as a person who:</Text>
+        <View style={SelfScreeningStyles.question1questions}>
+          <Text style={{fontSize: 18}}>o Provided care for the individual, including healthcare workers, family members or other caregivers, or who had similar close physical contact without consistent and appropriate use of personal protective equipments OR</Text>
+          <Text style={{fontSize: 18}}>o Who lived with or otherwise had close prolonged contact (within 6 feet/2 meters) with the person while they were infectious ORs</Text>
+          <Text style={{fontSize: 18}}>o Had direct contact with infectious bodily fluids of the person (e.g., was coughed or sneezed on) while not wearing recommended personal protective equipment</Text>
+        </View>
+      </View>
+    )
+  }
+
+  const answer = () => {
+    return (
+      <View style={SelfScreeningStyles.answer}>
+        <View style={SelfScreeningStyles.answerCheckBox}>
+          <CheckBox/>
+          <Text style={{fontSize: 18}}>Yes</Text>
+        </View>
+        <View style={SelfScreeningStyles.answerCheckBox}>
+          <CheckBox/>
+          <Text style={{fontSize: 18}}>No</Text>
+        </View>
+      </View>
+    )
+  }
+
   return (  
     <View>
       <ScrollView>
-      <Header label='Self-Screening Form' labelStyle={{}} back={true} navigation={navigation}/>
+        <Header label='Self-Screening Form' labelStyle={{}} back={true} navigation={navigation}/>
+        {question1()}
+        {answer()}
+        <View style={SelfScreeningStyles.break}/>
+        {question2()}
+        {answer()}
       </ScrollView>
+      <View style={SelfScreeningStyles.confirm}>
+        <Button label={'Complete Form'} onPress={()=>{}} labelStyle={{}} buttonStyle={{}}/>
+      </View>
     </View>
     )
 
--- components/SelfScreening/selfscreening-styles.ts
+++ components/SelfScreening/selfscreening-styles.ts
@@ -1,7 +1,53 @@
 import { StyleSheet } from 'react-native'
 
 const SelfScreeningStyles = StyleSheet.create({
+  question: {
+    paddingHorizontal: 33,
+    paddingVertical: 30,
+    fontSize: 18,
+    fontFamily: 'roboto'
+  },
+  questionTitle: {
+    fontWeight: '500',
+    marginBottom: 8,
+    fontSize: 18,
+  },
+  questionCTA: {
+    marginBottom: 12,
+    fontSize: 18
+  },
+  question1questions: {
+    marginLeft: 12,
+    fontSize: 18
+  },
+  question2desc: {
+    fontSize: 18,
+    textDecoration: 'underline'
 
+  },
+  break: {
+    backgroundColor: '#EBEAEA',
+    height: 2,
+    marginHorizontal: 27
+  },
+  answer: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginBottom: 30
+  },
+  answerCheckBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '14%',
+    marginLeft: 33,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  confirm: {
+    marginHorizontal: 30,
+    marginTop: 20,
+    marginBottom: 50
+  }
 })
 
 export default SelfScreeningStyles
